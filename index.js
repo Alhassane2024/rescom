1fe2915769f937fa39e7921c1c9500eca54b9b3c
Alhassane2024 <bah480286@gmail.com>
2024-06-20 22:05:20 +0000

    troisieme commit

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
             if (entry.isIntersecting) {
                 if (entry.target.classList.contains("slide_up")) {
                     entry.target.classList.add("visible");
+                } else if (entry.target.classList.contains("slide_left")) {
+                    entry.target.classList.add("visible_slide_left");
+                } else if (entry.target.classList.contains("slide_right")) {
+                    entry.target.classList.add("visible_slide_right");
                 }
+
 
 
                 intersectionAPI.unobserve(entry.target);
@@ -17,31 +22,13 @@ document.addEventListener("DOMContentLoaded", function () {
         intersectionAPI.observe(el);
     })
 
-
-    let index = 0;
-    const slides = document.querySelectorAll(".container_slider");
-    const totalSlides = slides.length;
-
-    const showSlide = (n) => {
-        if (n >= totalSlides) {
-            index = 0;
-        } else if (n < totalSlides) {
-            index = totalSlides - 1;
-        }
-
-        const sliderContainer = document.querySelector(".slider");
-        sliderContainer.style.transform = `translateX(${-index * 100}px)`;
-    }
-
-    document.querySelector(".prev").addEventListener("click", () => {
-        index--;
-        showSlide(index);
+    document.querySelectorAll(".slide_left").forEach(el => {
+        intersectionAPI.observe(el);
     })
 
-    document.querySelector(".next").addEventListener("click", () => {
-        index++;
-        console.log(index);
-        showSlide(index);
-        console.log(index);
+    document.querySelectorAll(".slide_right").forEach(el => {
+        intersectionAPI.observe(el);
     })
+
+
 })
